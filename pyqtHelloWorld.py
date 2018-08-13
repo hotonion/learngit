aa8ca8e0609cb4d969beae03cd5d3b064e6e2890
hotonion <hotonion@gmail.com>
2018-08-13 19:35:28 +0800

    add icon

--- pyqtHelloWorld.py
+++ pyqtHelloWorld.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget
-
+from PyQt5.QtGui import QIcon
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWinodw = QWidget()
-    mainWinodw.resize(300,400)
+    mainWinodw.resize(300,200)
     mainWinodw.move(400,300)
-    mainWinodw.setWindowTitle("TEST")
-    
+    mainWinodw.setWindowTitle("测试")
+    mainWinodw.setWindowIcon(QIcon("icon-96x96.png"))
+
+
     mainWinodw.show()
 
 

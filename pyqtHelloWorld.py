ca52034370d515bb7e42ceacf0afbafe12169c90
hotonion <hotonion@gmail.com>
2018-08-21 22:33:56 +0800

    登录框调试完成，可以显示主界面

--- pyqtHelloWorld.py
+++ pyqtHelloWorld.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtWidgets import QAction,QApplication, QWidget, QPushButton, QToolTip, QMessageBox,QDesktopWidget,QMainWindow,qApp
 from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
 
-class mainUi(QWidget):
+class mainUi(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUi()
@@ -13,7 +14,7 @@ class mainUi(QWidget):
     def initUi(self):
 
         QToolTip.setFont(QFont('SansSerif', 10))
-
+        #
         self.setToolTip('This is a <b>QWidget</b> widget 哈哈')
 
         self.setGeometry(300, 200, 400, 300)
@@ -25,7 +26,46 @@ class mainUi(QWidget):
         btn.setToolTip('This is a QPushButton widget')
         btn.resize(btn.sizeHint())
         btn.move(50, 50)
+
+        qbtn = QPushButton('&Quit', self)
+        qbtn.setToolTip('This is a QPushButton widget')
+        qbtn.resize(btn.sizeHint())
+        qbtn.move(50, 150)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+
+        self.statusBar().showMessage("Ready")
+        self.initMenu()
+        self.center()
         self.show()
+
+    def initMenu(self):
+        exitAction = QAction(QIcon('icon-96x96.png'),'&Exit',self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit app')
+        exitAction.triggered.connect(qApp.quit)
+        newAction = QAction('&Add',self)
+        newAction.setShortcut('Ctrl+A')
+        menubar = self.menuBar()
+        taskMenu = menubar.addMenu('&Task')
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        fileMenu.addAction(newAction)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+
+        retmsgbox = QMessageBox.question(self, 'Msg', "A U to Quit?",
+                                         QMessageBox.Yes | QMessageBox.No,
+                                         QMessageBox.Yes)
+        if retmsgbox == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == '__main__':
